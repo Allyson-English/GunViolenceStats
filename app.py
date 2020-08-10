f3bc420e6bfbfad6e9fa7900225f20249318336b
Allyson English <58833140+Allyson-English@users.noreply.github.com>
2020-08-10 00:21:55 -0400

    connecting site to database

--- app.py
+++ app.py
@@ -3,15 +3,28 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import datetime
+import sqlalchemy
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
+if False:
+    headers = headers
+
+    url = "https://www.gunviolencearchive.org/last-72-hours"
+
+    page = requests.get(url, headers=headers)
+    page = page.text
+
+    df = pd.read_html(page, header=0, index_col=0)
+    df = df[0].reset_index().drop(columns=["Operations"])
+    df.columns = ["date", "state", "city", "address", "killed", "injured"]
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
-    }
+    headers = headers
 
     url = "https://www.gunviolencearchive.org/last-72-hours"
 
@@ -34,6 +47,20 @@ def index():
     x = datetime.datetime.now().strftime("%B %d, %Y")
 
     return render_template("index.html", df=df, df_len=df_len, x=x, deaths=deaths, injuries=injuries, states=states)
+
+
+@app.route("/testing")
+def testing():
+    db_path = db_path
+    engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
+
+    with engine.connect() as conn:
+        query1 = """SELECT * FROM gun_violence WHERE state = 'New York';"""
+        dc_df = pd.read_sql(query1, conn)
+
+    dc_df_len = len(dc_df)
+
+    return render_template("testing.html", dc_df=dc_df, dc_df_len=dc_df_len)
 
 
 if __name__ == "__main__":
