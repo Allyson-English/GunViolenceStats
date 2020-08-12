c4aebd4249d3822cca4912450ed5f4e5a64676ec
English8 <allyson.english@gmail.com>
2020-08-11 23:14:30 -0400

    filtering data on webpage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from headers import headers, db_path
+from headers import db_path
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -25,23 +25,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     
-    today_month = datetime.datetime.today()
-    today_month = today_month.strftime('%B')
+    today = datetime.datetime.today()
+    today_month = today.strftime('%B')
+    today_date = int(today.strftime('%d'))
+    today_year = today.strftime('%Y')
 
-    today_date = datetime.datetime.today()
-    today_date = int(today_date.strftime('%d'))
-
-    today_year = datetime.datetime.today()
-    today_year = today_year.strftime('%Y')
-
-    yesterday_date = datetime.datetime.today() - datetime.timedelta(days=1)
-    yesterday_date = int(yesterday_date.strftime('%d'))
-
-    yesterday_month = datetime.datetime.today() - datetime.timedelta(days=1)
-    yesterday_month = yesterday_month.strftime('%B')
-
-    yesterday_year = datetime.datetime.today() - datetime.timedelta(days=1)
-    yesterday_year = yesterday_year.strftime('%Y')
+    yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
+    yesterday_month = yesterday.strftime('%B')
+    yesterday_date = int(yesterday.strftime('%d'))
+    yesterday_year = yesterday.strftime('%Y')
 
     today = f"{today_month} {today_date}, {today_year}"
     yesterday = f"{yesterday_month} {yesterday_date}, {yesterday_year}"
@@ -49,10 +41,10 @@ def index():
     engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
     
     with engine.connect() as conn:
-        query = f"SELECT * FROM gun_violence WHERE date = '{today}' OR date = '{yesterday}';"
+        query = f"SELECT * FROM gun_violence WHERE city = 'Washington' AND date = '{today}' OR city = 'Washington' AND date = '{yesterday}';"
         df = pd.read_sql(query, conn)
 
-    df = df.sort_values(by = 'date', ascending = True)
+    df = df.sort_values(by = 'state', ascending=True)
 
     df_len = len(df)
     deaths = df["killed"].sum()
@@ -65,6 +57,23 @@ def index():
 @app.route("/testing")
 def testing():
 
+    state_names = ["Alaska", 
+    "Alabama", "Arkansas",
+    "Arizona", "California", "Colorado", 
+    "Connecticut", "District of Columbia", 
+    "Delaware", "Florida", "Georgia", 
+    "Hawaii", "Iowa", "Idaho", "Illinois", 
+    "Indiana", "Kansas", "Kentucky", "Louisiana", 
+    "Massachusetts", "Maryland", "Maine", "Michigan", 
+    "Minnesota", "Missouri", "Mississippi", 
+    "Montana", "North Carolina", "North Dakota", 
+    "Nebraska", "New Hampshire", "New Jersey", 
+    "New Mexico", "Nevada", "New York", "Ohio", 
+    "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", 
+    "Rhode Island", "South Carolina", "South Dakota", 
+    "Tennessee", "Texas", "Utah", "Virginia", "Vermont", 
+    "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
     engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
 
     with engine.connect() as conn:
@@ -73,7 +82,7 @@ def testing():
 
     dc_df_len = len(dc_df)
 
-    return render_template("testing.html", dc_df=dc_df, dc_df_len=dc_df_len)
+    return render_template("testing.html", dc_df=dc_df, dc_df_len=dc_df_len, state_names=state_names)
 
 
 if __name__ == "__main__":
